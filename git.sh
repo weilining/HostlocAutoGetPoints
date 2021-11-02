07792c81e82574d6f6d778627b5bd54b40808f8e
weilining <18277278902@163.com>
2021-11-02 18:25:22 +0800

    main

--- git.sh
+++ git.sh
@@ -3,10 +3,3 @@ git add .
 git commit -m "main"
 git pull origin main
 git push origin main
-# 创建tag名为v1.0.8，并添加描述
-git tag -a "v1.0.12" -m '发布v1.0.12版本
-修复了以下bug:
-1. xxxxx
-2. xxxxx'
-# 把tag推到github上
-git push --tags
